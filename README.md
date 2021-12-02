d3fb85835c7fc3d5b1648dbac6c8faaab41669d5
Nicholas Dille <nicholas.dille@mailbox.org>
2021-12-02 12:02:53 +0100

    Updated with a fix using traefik v2.5

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sonarqube-java8u252-issue
 
 [First mention](https://community.sonarsource.com/t/connectionshutdownexception-in-maven-plugin-with-java-8-update-252/23913) by colleague Hans-Peter Keck
+
+Solved by [protocol negotiation in traefik](https://doc.traefik.io/traefik/v2.5/https/tls/#alpn-protocols)
--- run.sh
+++ run.sh
@@ -10,13 +10,12 @@ fi
 export SONARQUBE_DNS_NAME
 
 if ! test -d docker-maven; then
-    git clone https://github.com/carlossg/docker-maven.git docker-maven
+    git clone -q --depth 1 https://github.com/carlossg/docker-maven.git docker-maven
     cd docker-maven
-    git checkout ef95d59
     cp -r openjdk-8-slim openjdk-8u242-slim
-    sed -i 's/^FROM openjdk:8-jdk-slim/FROM openjdk:8u242-jdk-slim/' docker-maven/openjdk-8u242-slim/Dockerfile
+    sed -i 's/^FROM openjdk:8-jdk-slim/FROM openjdk:8u242-jdk-slim/' openjdk-8u242-slim/Dockerfile
     cp -r openjdk-8-slim openjdk-8u252-slim
-    sed -i 's/^FROM openjdk:8-jdk-slim/FROM openjdk:8u252-jdk-slim/' docker-maven/openjdk-8u252-slim/Dockerfile
+    sed -i 's/^FROM openjdk:8-jdk-slim/FROM openjdk:8u252-jdk-slim/' openjdk-8u252-slim/Dockerfile
     cd ..
 fi
 
@@ -28,11 +27,17 @@ while ! curl -s https://${SONARQUBE_DNS_NAME}/batch/index; do
 done
 
 docker-compose exec java8u242 bash -c 'apt-get update && apt-get -y install git'
-docker-compose exec --workdir /tmp java8u242 git clone https://github.com/SonarSource/sonar-scanning-examples
-docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u242 sed -i 's|<maven.compiler.release>11</maven.compiler.release>||' pom.xml
-docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u242 mvn clean verify sonar:sonar -Dsonar.host.url=https://${SONARQUBE_DNS_NAME}
+docker-compose exec --workdir /tmp java8u242 \
+    bash -c 'test -d /tmp/sonar-scanning-examples || git clone https://github.com/SonarSource/sonar-scanning-examples'
+docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u242 \
+    sed -i 's|<maven.compiler.release>11</maven.compiler.release>||' pom.xml
+docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u242 \
+    mvn clean verify sonar:sonar -Dsonar.host.url=https://${SONARQUBE_DNS_NAME}
 
 docker-compose exec java8u252 bash -c 'apt-get update && apt-get -y install git'
-docker-compose exec --workdir /tmp java8u252 git clone https://github.com/SonarSource/sonar-scanning-examples
-docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u252 sed -i 's|<maven.compiler.release>11</maven.compiler.release>||' pom.xml
-docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u252 mvn clean verify sonar:sonar -Dsonar.host.url=https://${SONARQUBE_DNS_NAME}
+docker-compose exec --workdir /tmp java8u252 \
+    bash -c 'test -d /tmp/sonar-scanning-examples || git clone https://github.com/SonarSource/sonar-scanning-examples'
+docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u252 \
+    sed -i 's|<maven.compiler.release>11</maven.compiler.release>||' pom.xml
+docker-compose exec --workdir /tmp/sonar-scanning-examples/sonarqube-scanner-maven/maven-basic java8u252 \
+    mvn clean verify sonar:sonar -Dsonar.host.url=https://${SONARQUBE_DNS_NAME}
